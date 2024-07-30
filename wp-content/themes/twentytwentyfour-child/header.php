4f7b3823df862a11ae74fede7341d352037df3d6
AlainArnault <alain.arnault89@gmail.com>
2024-07-30 09:46:41 +0200

    changement de theme

---
+++ wp-content/themes/twentytwentyfour-child/header.php
@@ -0,0 +1,9 @@
+<header>
+    <a href="http://localhost/planty/" class="logo-header" rel="home">
+        <img src="http://localhost/planty/wp-content/uploads/2024/07/Logo.svg" class="logo-header" alt="Planty" decoding="async">
+    </a>
+    <nav>
+         <?php
+         wp_nav_menu()
+
+    </nav>
