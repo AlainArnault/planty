b858a9ca1d83933be361dabe959f71fd1ce92276
AlainArnault <alain.arnault89@gmail.com>
2024-09-03 22:44:49 +0200

    commit final

--- wp-content/themes/twentytwentyfour-child/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <a href="http://localhost/planty/" class="logo-header" rel="home">
-        <img src="http://localhost/planty/wp-content/uploads/2024/07/Logo.svg" class="logo-header" alt="Planty" decoding="async">
-    </a>
-    <nav>
-         <?php
-         wp_nav_menu()
-
-    </nav>
